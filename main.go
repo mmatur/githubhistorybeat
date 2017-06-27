b2a74beb76e8403c7aa3b80dc9932c04ea4fc605
Michaël MATUR <michael.matur@gmail.com>
2017-06-27 17:22:25 +0200

    Add version and name for beat

--- main.go
+++ main.go
@@ -8,8 +8,11 @@ import (
 	"github.com/mmatur/githubhistorybeat/beater"
 )
 
+var version = "1.0.0-SNAPSHOT"
+var name = "githubhistorybeat"
+
 func main() {
-	err := beat.Run("githubhistorybeat", "", beater.New)
+	err := beat.Run(name, version, beater.New)
 	if err != nil {
 		os.Exit(1)
 	}
